99d0b3b834b519315684d14eded9d42285cf4532
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-01-08 11:19:50 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
+  let result = '';
+  let str = n.toString();
+  let arr = str.split('');
+
   
-}
+
+  for (let i = 0; i < str.length; i++) {
+    
+    result = arr[i] + result;
+
+    if(result[result.length - 1] === '-') {
+      result = result.slice(result.length);
+    }
+  }
+
+  return Number(result);
+};
